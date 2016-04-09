5cbd64aa74ab3e5433cc925dbbdf8f672d578435
Insane <luojieyy@gmail.com>
2016-04-09 22:47:52 +0800

    添加推送测试方法

--- gateway/push.js
+++ gateway/push.js
@@ -2,6 +2,24 @@ var JPush = require("jpush-sdk");
 var config = require('config');
 var client = JPush.buildClient(config.PUSHKEY, config.PUSHSECRET);
 
-exports.Send = function Send () {
+exports.Send = function Send (target, message) {
+	// target - push tag id -- project/user
+	// 
+	client.push().setPlatform('ios', 'android')
+    .setAudience(JPush.tag(target))
+    .setNotification(message.title, 
+    	JPush.ios('ios alert'), 
+    	JPush.android('android alert', null, 1)
+    )
+    .setMessage(message.content)
+    .setOptions(null, 60)
+    .send(function(err, res) {
+        if (err) {
+            console.log(err.message);
+        } else {
+            console.log('Sendno: ' + res.sendno);
+            console.log('Msg_id: ' + res.msg_id);
+        }
+    });
 
 }
