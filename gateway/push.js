6790381eaaea504a80081d1505c24bb630a26523
Insane <luojieyy@gmail.com>
2016-04-24 23:24:26 +0800

    更新发送方式

--- gateway/push.js
+++ gateway/push.js
@@ -2,21 +2,27 @@ var JPush = require("jpush-sdk");
 var config = require('config');
 var client = JPush.buildClient(config.PUSHKEY, config.PUSHSECRET);
 
+// target : user_2132010105 / project_56713c947f0fd05911d382f4
+// message : {
+//     content : 'test',
+//     title : '账户充值推送通知：您云能源账户已成功充值220元人民币，查看账户详情'
+// }
+
 exports.Send = function Send (target, message) {
-	// target - push tag id -- project/user
-	// 
-	client.push().setPlatform('ios', 'android')
+    // target - push tag id -- project/user
+    // 
+    client.push().setPlatform('ios', 'android')
     .setAudience(JPush.tag(target))
-    .setNotification('云能源通知消息', // 全局消息名称  
-    	JPush.ios('ios alert', 'sound', 1, false, {
+    .setNotification(
+        JPush.ios(message.title, 'sound', 1, false, {
             action: 'pay'   // 根据不同事件修改 action
         }), 
-    	JPush.android('android alert', null, 1, {
+        JPush.android(message.title, null, 1, {
             action: 'pay'   // 同上
         })
     )
     .setMessage(message.content)
-    .setOptions(null, 60)
+    .setOptions(null, 60, null, true)
     .send(function(err, res) {
         if (err) {
             console.log(err.message);
