d5c90eaa172665171fec5f3a93d0e07238a3a5b4
Insane <luojieyy@gmail.com>
2016-05-29 00:14:33 +0800

    添加附件文件名处理

--- gateway/email.js
+++ gateway/email.js
@@ -1,13 +1,13 @@
-var email = require('emailjs'),
-    emailComposer = require('../libs/email'),
-    _ = require('underscore'),  
+'use strict';
+var emailComposer = require('../libs/email'),
+    handlebars = require('handlebars'),  
     nodemailer = require('nodemailer');
 
-var user = "古鸽云能源 <group@cloudenergy.me>";
+var user = '古鸽云能源 <group@cloudenergy.me>';
 
 var transporter = nodemailer.createTransport({
     port: 465,
-    host: "smtp.exmail.qq.com",
+    host: 'smtp.exmail.qq.com',
     secure: true,
     auth: {
         user: 'group@cloudenergy.me',
@@ -24,30 +24,34 @@ exports.Init = function () {
 exports.Send = function(target, msg){
 
     var event = msg.event,
+        template = msg.template,
         event_data = msg.data,
         param = event_data.get('param'),
+        ext = (template.email && template.email.ext) || 'xlsx',
         file = param.file;
 
      var mailOptions = {
         from: user,
         to: target,
-        subject: msg.template.title,
+        subject: template.title,
         html: emailComposer.compile(event, param),
-    }
+    };
 
     // 附件
     if (file) {
+        var file_name = template.title;
+        if (template.email.title) {
+            file_name = handlebars.compile(template.email.title)(param);
+        }
+
         mailOptions.attachments = [
             {   // use URL as an attachment
-                filename: '报表.xlsx',
+                filename:  `${file_name}.${ext}`,
                 path: file
             }
-        ]
+        ];
     }
 
-    // console.log('opt: ', mailOptions);
-
-    // return
     transporter.sendMail(mailOptions, function(error, info){
         if(error){
             return console.log('error: ', error, info);
